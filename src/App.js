afc19446b716163ad3d7f708104529951e8c5a5d
Jackie <jonofre16@hotmail.com>
2018-06-19 10:32:18 -0700

    Added header to app component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchCollection from './components/SearchCollection';
-// import { Router, Route, Switch, Link } from 'react-router'
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
-import Search from './components/Search'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Video Store</h1>
+          <div>Search</div>
+          <div>Library</div>
+          <div>Customers</div>
+          <div>Selected Movie</div>
+          <div>Selected Customer</div>
         </header>
+        < SearchCollection />
       </div>
     );
   }
