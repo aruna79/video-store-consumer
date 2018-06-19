ed242354a289ed40aa19528d272d28d53a5bda88
Jackie <jonofre16@hotmail.com>
2018-06-19 10:22:47 -0700

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchCollection from './components/SearchCollection';
+// import { Router, Route, Switch, Link } from 'react-router'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import Search from './components/Search'
 
 class App extends Component {
   render() {
@@ -9,12 +12,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Video Store</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <SearchCollection />
       </div>
     );
   }
