99a31610266983d85dd348891f9e755d3302251c
Aruna <aruna.thungathurthi@gmail.com>
2018-06-19 11:11:37 -0700

    added react routes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,41 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
 import {
-    Router,
-    Route,
-    Link
-} from 'react-router'
-
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
-=======
 import SearchCollection from './components/SearchCollection';
-// import { Router, Route, Switch, Link } from 'react-router'
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
-import Search from './components/Search'
->>>>>>> ed242354a289ed40aa19528d272d28d53a5bda88
+import CustomerCollection from './components/CustomerCollection';
+
 
 class App extends Component {
+
   render() {
 
     const home = () => {
       return (<p>Welcome!</p>);
     };
 
+
     return (
-<<<<<<< HEAD
-        <Router>
-          <section>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              {/*<li><Link to="/search">Search</Link></li>*/}
-              {/*<li><Link to="/library">Library</Link></li>*/}
-              {/*<li><Link to="/customers">Customers</Link></li>*/}
-            </ul>
+      <Router>
+      <section>
+      <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/search">Search</Link></li>
+      <li><Link to="/customers">CustomerCollection</Link></li>
+      </ul>
 
-            <hr/>
 
-            <Route exact path="/" component={home}/>
-            {/*<Route path="/search" component={Search}/>*/}
-            {/*<Route path="/library" component={Library}/>*/}
-            {/*<Route path="/customers" component={Customers}/>*/}
-          </section>
-        </Router>
-=======
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Video Store</h1>
-        </header>
-      </div>
->>>>>>> ed242354a289ed40aa19528d272d28d53a5bda88
+
+      <Route exact path="/" component={home}/>
+      <Route path="/search" component={SearchCollection}/>
+      <Route path="/customers" component={CustomerCollection}/>
+      </section>
+      </Router>
     );
   }
 }
